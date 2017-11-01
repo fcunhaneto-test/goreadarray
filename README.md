aebeba0a2430ed2b527e53d0a7765c8c0a1d1fd9
Francisco Cunha Neto <fcunhaneto@gmail.com>
2017-11-01 10:25:42 -0200

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # goreadarray
-Goreadarray package to read a simple array with float 64 values from a file or through the terminal.
+
+Package goreadarray to read a simple array with float64 values through the terminal.
+
+______________________________________________________________________________________
+Pacote goreadarray para ler uma array simples com valores float64 através do terminal.
+
+## Termread()
+
+Reads an array through the terminal, where the elements are separated by space.
+
+Example:
+
+$ Enter array:<br />
+$ 2.4 6.8 7.2 10.6 5.9<br />
+
+_______________________________________________________________________________
+Lê uma matriz através do terminal, onde os elementos são separados pelo espaço.
+
+Examplo:
+
+$ Enter array:<br />
+$ 2.4 6.8 7.2 10.6 5.9<br />
---
+++ readarray.go
@@ -0,0 +1,61 @@
+/*
+Package goreadarray to read a simple array with float 64 values from a file or through the terminal.
+*/
+package goreadarray
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+Termread reads an array through the terminal, where the elements are separated by space.
+
+Example:
+
+# Enter array:
+# 2.4 6.8 7.2 10.6 5.9
+
+return:
+a []float64: array
+*/
+func Termread() []float64 {
+	var a []float64
+	var line []string
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Enter array: ")
+	text, _ := reader.ReadString('\n')
+	text = text[:len(text)-1]
+
+	line = strings.Split(string(text), " ")
+	a = lineToFloat(line)
+
+	return a
+}
+
+/*
+lineToFloat transforms an array of strings into an array of float64
+
+param:
+line: []string
+
+return:
+s: []float64
+*/
+func lineToFloat(line []string) []float64 {
+	var s = []float64{}
+
+	for _, l := range line {
+		num, err := strconv.ParseFloat(l, 64)
+		if err != nil {
+			panic(err)
+		}
+		s = append(s, num)
+	}
+
+	return s
+}
